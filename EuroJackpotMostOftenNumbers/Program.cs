b85629ace95d723faa3898cf04e8bd0985d6245e
Marcin Gadomski <marcingadomski94@gmail.com>
2018-11-02 22:05:16 +0100

    ulepszenia

--- EuroJackpotMostOftenNumbers/Program.cs
+++ EuroJackpotMostOftenNumbers/Program.cs
@@ -9,91 +9,116 @@ using Newtonsoft.Json;
 
 namespace EuroJackpotMostOftenNumbers
 {
-	class Program
-	{
-		private static Dictionary<int, int> _numbersCount;
-		private static Dictionary<int, int> _numbersCountSorted;
-		private static Dictionary<int, int> _starNumbersCount;
-		private static Dictionary<int, int> _starNumbersCountSorted;
+    class Program
+    {
+        private static Dictionary<int, int> _numbersCount;
+        private static Dictionary<int, int> _numbersCountSorted;
+        private static Dictionary<int, int> _starNumbersCount;
+        private static Dictionary<int, int> _starNumbersCountSorted;
+        private static List<int> _last3Months;
 
-		private static readonly int MAX_NUMBER = 50;
-		private static readonly int MAX_STAR_NUMBER = 10;
+        private static readonly int MAX_NUMBER = 50;
+        private static readonly int MAX_STAR_NUMBER = 10;
 
-		static void Main(string[] args)
-		{
-			_numbersCount = new Dictionary<int, int>();
-			_numbersCountSorted = new Dictionary<int, int>();
-			_starNumbersCount = new Dictionary<int, int>();
-			_starNumbersCountSorted = new Dictionary<int, int>();
+        static void Main(string[] args)
+        {
+            _numbersCount = new Dictionary<int, int>();
+            _numbersCountSorted = new Dictionary<int, int>();
+            _starNumbersCount = new Dictionary<int, int>();
+            _starNumbersCountSorted = new Dictionary<int, int>();
+            _last3Months = new List<int>();
+            
 
-			var httpClient = new HttpClient();
-			httpClient.DefaultRequestHeaders.Accept.Clear();
-			httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-			var drawDate = new DateTime(2015, 01, 02);
-			var draws = 0;
+            var drawDate = new DateTime(2015, 01, 02);
+            var draws = 0;
 
-			var numberToAdd = 1;
-			while (numberToAdd <= MAX_NUMBER)
-			{
-				_numbersCount.Add(numberToAdd, 0);
-				numberToAdd++;
-			}
+            var numberToAdd = 1;
+            while (numberToAdd <= MAX_NUMBER)
+            {
+                _numbersCount.Add(numberToAdd, 0);
+                numberToAdd++;
+            }
 
-			var starNumberToAdd = 1;
-			while (starNumberToAdd <= MAX_STAR_NUMBER)
-			{
-				_starNumbersCount.Add(starNumberToAdd, 0);
-				starNumberToAdd++;
-			}
+            var starNumberToAdd = 1;
+            while (starNumberToAdd <= MAX_STAR_NUMBER)
+            {
+                _starNumbersCount.Add(starNumberToAdd, 0);
+                starNumberToAdd++;
+            }
 
-			while (drawDate < DateTime.Now)
-			{
-				draws++;
-				Console.WriteLine($"Checking result for {drawDate.ToString("yyyy-MM-dd")}");
-				var externalRequest = new EuroJackpotRequest();
-				externalRequest.Date = drawDate;
-				var externalRequestString = externalRequest.ToString();
-				var httpResponse = httpClient.GetAsync(externalRequestString).Result;
-				var externalResponseString = httpResponse.Content.ReadAsStringAsync().Result;
-				var externalResponse = JsonConvert.DeserializeObject<EuroJackpotResponse>(externalResponseString);
+            while (drawDate < DateTime.Now)
+            {
+                draws++;
+                Console.WriteLine($"Checking result for {drawDate.ToString("yyyy-MM-dd")}");
+                var externalRequest = new EuroJackpotRequest();
+                externalRequest.Date = drawDate;
+                var externalRequestString = externalRequest.ToString();
+                var httpResponse = httpClient.GetAsync(externalRequestString).Result;
+                var externalResponseString = httpResponse.Content.ReadAsStringAsync().Result;
+                var externalResponse = JsonConvert.DeserializeObject<EuroJackpotResponse>(externalResponseString);
 
-				foreach (var winningNumber in externalResponse.winningNumbers)
-				{
-					if (_numbersCount.ContainsKey(winningNumber))
-						_numbersCount[winningNumber] = _numbersCount[winningNumber] + 1;
-					else
-						_numbersCount.Add(winningNumber, 1);
-				}
+                if (externalResponse.winningNumbers != null)
+                {
+                    foreach (var winningNumber in externalResponse.winningNumbers)
+                    {
+                        if (_numbersCount.ContainsKey(winningNumber))
+                            _numbersCount[winningNumber] = _numbersCount[winningNumber] + 1;
+                        else
+                            _numbersCount.Add(winningNumber, 1);
+                        
+                        if((DateTime.Now.Year == drawDate.Year) && (DateTime.Now.Month - 2 <= drawDate.Month))
+                            _last3Months.Add(winningNumber);
+                    }
+                }
 
-				foreach (var starNumber in externalResponse.starNumbers)
-				{
-					if (_starNumbersCount.ContainsKey(starNumber))
-						_starNumbersCount[starNumber] = _starNumbersCount[starNumber] + 1;
-					else
-						_starNumbersCount.Add(starNumber, 1);
-				}
+                if (externalResponse.starNumbers != null)
+                {
+                    foreach (var starNumber in externalResponse.starNumbers)
+                    {
+                        if (_starNumbersCount.ContainsKey(starNumber))
+                            _starNumbersCount[starNumber] = _starNumbersCount[starNumber] + 1;
+                        else
+                            _starNumbersCount.Add(starNumber, 1);
+                    }
+                }
+                
+                
 
-				drawDate = drawDate.AddDays(7);
-			}
+                drawDate = drawDate.AddDays(7);
+            }
 
-			foreach(var item in _numbersCount.OrderByDescending(a => a.Value))
-				_numbersCountSorted.Add(item.Key, item.Value);
+            foreach (var item in _numbersCount.OrderByDescending(a => a.Value))
+                _numbersCountSorted.Add(item.Key, item.Value);
 
-			foreach(var item in _starNumbersCount.OrderByDescending(a => a.Value))
-				_starNumbersCountSorted.Add(item.Key, item.Value);
+            foreach (var item in _starNumbersCount.OrderByDescending(a => a.Value))
+                _starNumbersCountSorted.Add(item.Key, item.Value);
 
-			Console.WriteLine(Environment.NewLine);
+            Console.WriteLine(Environment.NewLine);
 
-			foreach(var item in _numbersCountSorted)
-				Console.WriteLine($"Number {item.Key} was chosen {item.Value} times in {draws} draws");
+            foreach (var item in _numbersCountSorted)
+            {
+                if (_last3Months.Where(a => a.Equals(item.Key)).ToList().Count() >= 3)
+                    Console.ForegroundColor = ConsoleColor.Red;
+                else if (_last3Months.Where(a => a.Equals(item.Key)).ToList().Count() == 2)
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                else if (_last3Months.Where(a => a.Equals(item.Key)).ToList().Count() == 1)
+                    Console.ForegroundColor = ConsoleColor.Green;
+                else
+                    Console.ForegroundColor = ConsoleColor.White;
+                
+                Console.WriteLine($"Number {item.Key} was chosen {item.Value} times in {draws} draws");
+            }
 
-			Console.WriteLine(Environment.NewLine);
+            Console.WriteLine(Environment.NewLine);
 
-			foreach(var item in _starNumbersCountSorted)
-				Console.WriteLine($"Star number {item.Key} was chosen {item.Value} times in {draws} draws");
+            foreach (var item in _starNumbersCountSorted)
+                Console.WriteLine($"Star number {item.Key} was chosen {item.Value} times in {draws} draws");
 
-			Console.ReadLine();
-		}
-	}
+            Console.ReadLine();
+        }
+    }
 }
